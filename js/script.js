c967f6725e208a5eb17cdb875216b8fa507f8c3d
Sophia Goussetis <scg58@cabrini.edu>
2020-12-04 21:49:59 -0500

    help from Professor C

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-@import url('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js');
-@import url('https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js');
-
 $(".main-carousel").flickity({
     // options
     cellAlign: "left",
